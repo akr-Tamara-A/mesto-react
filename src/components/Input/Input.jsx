53d9da64c09af7c80f7288e37e3ee47ec0527995
Tamara A <akira-49-q@yandex.ru>
2020-09-13 18:31:57 +0300

    fix: исправить регистр названия папок Input и Spinner

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+/** Компонент "Инпут" */
+export default function Input(props) {
+  /** Разметка инпута */
+  return (
+    <label className="popup__form-field">
+      <input
+        id={`input-${props.name}`}
+        type={props.type}
+        required={props.isRequired}
+        minLength={props.minLength}
+        maxLength={props.maxLength}
+        pattern={props.pattern}
+        placeholder={props.placeholder}
+        className="popup__input"
+      />
+      <span id={`input-${props.name}-error`} className="popup__input-error"></span>
+    </label>
+  );
+}
