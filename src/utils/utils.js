ee242088ca3d123bdda6ffc4d4fa27e639b4d625
Tamara A <akira-49-q@yandex.ru>
2020-09-14 10:56:55 +0300

    refactor: убрать неиспользуемые функции из Utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,17 +1,1 @@
 /** Уведомление пользователя о процессе загрузки */
-export function renderLoading(isLoading, button) {
-  if (isLoading) {
-    button.value = "Сохранение...";
-  } else {
-    button.value = "Сохранить";
-  }
-}
-
-/** Уведомление пользователя о процессе удаления */
-export function renderDeletion(isLoading, button) {
-  if (isLoading) {
-    button.value = "Удаление...";
-  } else {
-    button.value = "Да";
-  }
-}
